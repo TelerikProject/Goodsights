939b31f1aa9cb35aa5f1295be53aafd651bfcc4e
Mihaela <mihaela.ivanova.ivanova@gmail.com>
2017-04-29 23:55:52 +0300

    Minor corrections on router - now working properly Needs to be extracted in another file

--- js/Router.js
+++ js/Router.js
@@ -1,65 +1,66 @@
 let container = $('#container');
-
 class MyRouter {
-    constructor() {
-        this._routes = [];
+  constructor() {
+    this._routes = [];
+  }
+
+  on(targetUrl, callback) {
+    this._routes.push({
+      targetUrl,
+      callback
+    });
+
+    return this;
+  }
+
+  navigate() {
+    const currentUrl = location.hash.slice(1);
+
+    for(const {targetUrl, callback} of this._routes) {
+      const params = MyRouter.matchUrls(currentUrl, targetUrl);
+      if(params) {
+        callback(params);
+        break;
+      }
+    }
+  }
+
+  static matchUrls(currentUrl, targetUrl) {
+    const currentUrlParts = currentUrl.split(/\//g);
+    const targetUrlParts = targetUrl.split(/\//g);
+
+    if(targetUrlParts.length !== currentUrlParts.length) {
+      return false;
     }
 
-    on(targetUrl, callback) {
-        this._routes.push({
-            targetUrl,
-            callback
-        });
-        return this;
-    }
+    const params = {};
 
-    navigate() {
-        const currentUrl = location.hash.slice(1);
-
-        for (const { targetUrl, callback } of this._routes) {
-            const params = MyRouter.matchUrls(currentUrl, targetUr)
-            if (params) {
-                callback(params);
-                break;
-            }
+    const len = currentUrlParts.length;
+    for(let i = 0; i < len; i += 1) {
+      if(targetUrlParts[i][0] !== ':') {
+        if(currentUrlParts[i] !== targetUrlParts[i]) {
+          return false;
         }
+      }
+      else {
+        const paramName = targetUrlParts[i].slice(1);
+        params[paramName] = currentUrlParts[i];
+      }
     }
 
-    static matchUrls(currentUrl, targetUrl) {
-
-        const currentUrlParts = currentUrl.split(/\//g);
-        const targetUrlParts = targetUrl.split(/\//g);
-
-        if (targetUrlParts.length !== length) {
-            return false;
-        }
-
-        const params = {};
-
-        const length = currentUrlParts.length;
-        for (let i = 0; i < length; i += 1) {
-            if (targetUrlParts[i][0] !== ":") {
-                if (currentUrlParts[i] !== targetUrlParts[i]) {
-                    return false;
-                }
-            } else {
-                const paramName = targetUrlParts[i].slice(1);
-                params[paramName] = currentUrlParts[i];
-            }
-        }
-        return params;
-    }
+    return params;
+  }
 }
 
 const router = new MyRouter();
 router
-    .on('/home', () => { container.htm('home'); console.log('something'); })
-    .on('/users', () => container.htm('users'))
-    .on('/users/:username', (params) => container.htm('Showing info for current user'))
-    .on('/sign-in', () => container.htm('sign-in'))
-    .on('/sign-up', () => container.htm('sign-up'))
+    .on('/home', () =>  container.html('home'))
+    .on('/users', () => container.html('users'))
+    .on('/users/:username', (params) => container.html('Showing info for current user'))
+    .on('/sign-in', () => container.html('sign-in'))
+    .on('/sign-up', () => container.html('sign-up'))
 
-$(window).on('load', () => router.navigate);
-$(window).on('hashchange', () => router.navigate);
+$(window).on('load', () => router.navigate());
+$(window).on('hashchange', () => router.navigate());
 
 
