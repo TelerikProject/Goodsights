0d440b20155ec073a6cf32f41ec85a7faa233841
ggeneva <g.geneva94@gmail.com>
2017-05-07 14:49:37 +0300

    Bases of the application.

--- public/scripts/models/router.js
+++ public/scripts/models/router.js
@@ -1,57 +1,54 @@
-let Router = (function () {
-  class Router {
-    constructor() {
-      this._routes = [];
-    }
+class MyRouter {
+  constructor() {
+    this._routes = [];
+  }
 
-    on(targetUrl, callback) {
-      this._routes.push({
-        targetUrl,
-        callback
-      });
+  on(targetUrl, callback) {
+    this._routes.push({
+      targetUrl,
+      callback
+    });
 
-      return this;
-    }
+    return this;
+  }
 
-    navigate() {
-      const currentUrl = location.hash.slice(1);
+  navigate() {
+    const currentUrl = location.hash.slice(1);
 
-      for (const { targetUrl, callback } of this._routes) {
-        const params = MyRouter.matchUrls(currentUrl, targetUrl);
-        if (params) {
-          callback(params);
-          break;
-        }
+    for(const {targetUrl, callback} of this._routes) {
+      const params = MyRouter.matchUrls(currentUrl, targetUrl);
+      if(params) {
+        callback(params);
+        break;
       }
-    }
-
-    static matchUrls(currentUrl, targetUrl) {
-      const currentUrlParts = currentUrl.split(/\//g);
-      const targetUrlParts = targetUrl.split(/\//g);
-
-      if (targetUrlParts.length !== currentUrlParts.length) {
-        return false;
-      }
-
-      const params = {};
-
-      const len = currentUrlParts.length;
-      for (let i = 0; i < len; i += 1) {
-        if (targetUrlParts[i][0] !== ':') {
-          if (currentUrlParts[i] !== targetUrlParts[i]) {
-            return false;
-          }
-        }
-        
-        else {
-          const paramName = targetUrlParts[i].slice(1);
-          params[paramName] = currentUrlParts[i];
-        }
-      }
-
-      return params;
     }
   }
-})();
 
+  static matchUrls(currentUrl, targetUrl) {
+    const currentUrlParts = currentUrl.split(/\//g);
+    const targetUrlParts = targetUrl.split(/\//g);
 
+    if(targetUrlParts.length !== currentUrlParts.length) {
+      return false;
+    }
+
+    const params = {};
+
+    const len = currentUrlParts.length;
+    for(let i = 0; i < len; i += 1) {
+      if(targetUrlParts[i][0] !== ':') {
+        if(currentUrlParts[i] !== targetUrlParts[i]) {
+          return false;
+        }
+      }
+      else {
+        const paramName = targetUrlParts[i].slice(1);
+        params[paramName] = currentUrlParts[i];
+      }
+    }
+
+    return params;
+  }
+}
+
+export { MyRouter };
--- public/scripts/system-config.js
+++ public/scripts/system-config.js
@@ -3,15 +3,27 @@ SystemJS.config({
     transpiler: 'plugin-babel',
     // tell SystemJS where to look for the dependencies
     map: {
+        //System.js files
         'plugin-babel': 'libs/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': 'libs/systemjs-plugin-babel/systemjs-babel-browser.js',
-        // app start script
-        'main': 'main.js',
-        'router':'models/router.js',
+        'text': 'libs/systemjs-plugin-text/text.js',
+        
+        // App files
+        'app': 'scripts/app.js',
+        'myRouter': 'scripts/models/router.js',
+        'requester': 'scripts/requester.js',
+        'data': 'scripts/userRequester.js',
+        'multipleController': 'scripts/controllers/multiple.js',
+        'myCookieController': 'scripts/controllers/myCookie.js',
+        'userController': 'scripts/controllers/user.js',
+        'templates': 'scripts/templates.js',
 
         //Library files
-        'jquery': 'libs/jquery/dist/jquery.min.js'
+        'jquery': 'libs/jquery/dist/jquery.min.js',
+        'handlebars': 'libs/handlebars/dist/handlebars.amd.js',
+        'cryptojs': 'libs/cryptojs/cryptojs.js',
+        'toastr': 'libs/toastr/build/toastr.min.js',
     }
 });
 
-//System.import('main');
+System.import('app');
